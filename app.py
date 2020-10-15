f0c049cabe065d6092512878f66fd75659e82420
sabattle <sabattle@eckerd.edu>
2020-10-15 00:53:48 -0400

    add random dog pics

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
-def hello_world():
-    return 'Hello World!'
+def get_dog():
+    res = requests.get('https://random.dog/woof.json')
+    image_url = res.json()['url']
+    return render_template('index.html', image=image_url)
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
